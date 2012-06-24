6aaafdb9e3f6ac11e546bb63e13654e08419356d
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-06-24 19:26:54 +0200

    Remove lib folder

--- test/buster.js
+++ test/buster.js
@@ -3,7 +3,7 @@ var config = module.exports;
 config["My tests"] = {
     rootPath: "../",
     environment: "browser",
-    src: ["lib/responseFaker.js"],
+    src: ["responseFaker.js"],
     specs: ["test/*-spec.js"],
     extensions: [require("sinon")]
 };
